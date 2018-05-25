ac0ec8fba8b14f20707bef5cf1cbec8dd4fa2c5d
vanpouckesven <sven.vanpoucke@hogent.be>
2018-05-25 10:01:06 +0200

    Initial commit + added GW2 site

---
+++ gw2/data/WorldBossSpawnTimes.js
@@ -0,0 +1,119 @@
+var tequatl = {
+  'name': 'Tequatl',
+  'map': 'Sparkfly Fen',
+  'location': 'Splintered Coast',
+  'waypoint': 'Splintered Coast Waypoint',
+  'waypoint_code': '[&BNABAAA=]',
+  'link': 'https://wiki.guildwars2.com/wiki/Defeat_Tequatl_the_Sunless'
+};
+
+var evolvedJungleWurm = {
+  'name': 'Evolved Jungle Wurm',
+  'map': 'Bloodtide Coast',
+  'location': 'Firth of Revanion',
+  'waypoint': 'Firthside Vigil Waypoint',
+  'waypoint_code': '[&BKoBAAA=]',
+  'link': 'https://wiki.guildwars2.com/wiki/Triple_Trouble'
+};
+
+var karkaQueen = {
+  'name': 'Karka Queen',
+  'map': 'Southsun Cove',
+  'location': 'Driftglass Springs',
+  'waypoint': ' Pearl Islet Waypoint',
+  'waypoint_code': '[&BNUGAAA=]',
+  'link': 'https://wiki.guildwars2.com/wiki/Defeat_the_Karka_Queen_threatening_the_settlements'
+};
+
+var worldBossSpawnTimes = [
+  {
+    'hour': 1,
+    'minutes': 0,
+    'boss': karkaQueen
+  },
+  {
+    'hour': 2,
+    'minutes': 0,
+    'boss': tequatl
+  },
+  {
+    'hour': 3,
+    'minutes': 0,
+    'boss': evolvedJungleWurm
+  },
+  {
+    'hour': 4,
+    'minutes': 0,
+    'boss': karkaQueen
+  },
+  {
+    'hour': 5,
+    'minutes': 0,
+    'boss': tequatl
+  },
+  {
+    'hour': 6,
+    'minutes': 0,
+    'boss': evolvedJungleWurm
+  },
+  {
+    'hour': 8,
+    'minutes': 0,
+    'boss': karkaQueen
+  },
+  {
+    'hour': 9,
+    'minutes': 0,
+    'boss': tequatl
+  },
+  {
+    'hour': 10,
+    'minutes': 0,
+    'boss': evolvedJungleWurm
+  },
+  {
+    'hour': 12,
+    'minutes': 30,
+    'boss': karkaQueen
+  },
+  {
+    'hour': 13,
+    'minutes': 30,
+    'boss': tequatl
+  },
+  {
+    'hour': 14,
+    'minutes': 30,
+    'boss': evolvedJungleWurm
+  },
+  {
+    'hour': 17,
+    'minutes': 0,
+    'boss': karkaQueen
+  },
+  {
+    'hour': 18,
+    'minutes': 0,
+    'boss': tequatl
+  },
+  {
+    'hour': 19,
+    'minutes': 0,
+    'boss': evolvedJungleWurm
+  },
+  {
+    'hour': 20,
+    'minutes': 0,
+    'boss': karkaQueen
+  },
+  {
+    'hour': 21,
+    'minutes': 0,
+    'boss': tequatl
+  },
+  {
+    'hour': 22,
+    'minutes': 0,
+    'boss': evolvedJungleWurm
+  }
+]
